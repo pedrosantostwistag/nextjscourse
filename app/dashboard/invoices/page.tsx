fc272ecabe0a0400a09357975b7baca8b44f8e6c
Pedro Santos <pedro.santos@twistag.com>
2025-01-03 15:17:09 +0000

    Add metadata template to the dashboard

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,13 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
- import { fetchInvoicesPages } from '@/app/lib/data';
+import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+// this metadata will override the parent metadata
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page(props: {
   // Table component is server side, use searchParams instead of useSearchParams hook 
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,18 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- export const experimental_ppr = true;
+import { Metadata } from 'next';
+export const experimental_ppr = true;
+ 
+
+// Next will add this title and description automatically to the page
+// We use a template, so what is passed to the title on child components will be appended to the template
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Mega Dashboard',
+    default: 'Mega Dashboard',
+  },
+  description: 'Super Dashboard with one bug or two',
+  metadataBase: new URL('https://thisLinkDoesNotExist.sh'),
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
